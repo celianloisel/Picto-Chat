207a66393292cfeefb46f122c8c9f93f28fadd41
celianloisel <celianloisel@gmail.com>
2023-04-12 09:00:05 +0200

    handle message + connection

--- server/Class/ChatServer.py
+++ server/Class/ChatServer.py
@@ -1,10 +1,14 @@
 import socket
+import threading
+
 
 class ChatServer:
     MAX_CONNECTIONS = 5
 
     def __init__(self, port):
         self._port = port
+        self._connections_thread = threading.Thread(target=self._accept_connections)
+        self._messages_thread = threading.Thread(target=self._handle_messages)
 
     def get_port(self):
         return self._port
@@ -12,24 +16,33 @@ class ChatServer:
     def set_port(self, new_value):
         self._port = new_value
 
-    def execute(self):
-        # Création d'un objet de socket
+    def _accept_connections(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-            # Liaison du socket à une adresse IP (10.57.32.3) et un port spécifique
-            server_socket.bind(('10.57.32.3', int(self._port)))
-            # Mise en écoute du socket avec un nombre maximum de connexions acceptées (5)
+            server_socket.bind(('10.57.33.126', int(self._port)))
             server_socket.listen(ChatServer.MAX_CONNECTIONS)
-            print(f"Le serveur écoute sur le port {self._port}")
+            print(f"Server is listening on port {self._port}")
             while True:
                 try:
-                    # Acceptation des connexions entrantes des clients
                     client_socket, client_address = server_socket.accept()
-                    print(f"Connexion établie depuis {client_address}")
-                    # Ajouter du code pour gérer les connexions clients
-                except KeyboardInterrupt:
-                    # Gestion de l'arrêt du serveur par l'utilisateur
-                    print("Arrêt du serveur par l'utilisateur.")
-                    break
+                    print(f"Connection established from {client_address}")
+
+                    # Traiter la connexion client ici
+
+                    print(f"Client {client_address} disconnected")
+
                 except Exception as e:
-                    # Gestion des autres exceptions possibles
-                    print(f"Erreur : {e}")
+                    print(f"Error accepting connection: {e}")
+
+    def _handle_messages(self):
+        while True:
+            # Traiter les messages reçus des clients ici
+            pass
+
+    def execute(self):
+        self._connections_thread.start()
+        self._messages_thread.start()
+
+        self._connections_thread.join()
+        self._messages_thread.join()
+
+        print("Server stopped.")
--- server/client.py
+++ server/client.py
@@ -1,7 +1,7 @@
 import socket
 
 # Adresse IP et port du serveur de chat
-SERVER_IP = '10.57.32.3'
+SERVER_IP = '10.57.33.126'
 SERVER_PORT = 2222
 
 # Création d'un objet de socket pour le client
@@ -12,12 +12,12 @@ try:
     client_socket.connect((SERVER_IP, SERVER_PORT))
     print("Connexion établie avec le serveur.")
 
-    # Envoyer des données au serveur
-    client_socket.sendall(b"Hello, serveur!")
-
-    # Recevoir les données du serveur
-    data = client_socket.recv(1024)
-    print(f"Reçu du serveur : {data.decode()}")
+    # # Envoyer des données au serveur
+    # client_socket.sendall(b"Hello, serveur!")
+    #
+    # # Recevoir les données du serveur
+    # data = client_socket.recv(1024)
+    # print(f"Reçu du serveur : {data.decode()}")
 
     # Fermer la connexion
     client_socket.close()
